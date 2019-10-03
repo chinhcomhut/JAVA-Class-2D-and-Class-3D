369eb1184cc59aa900362ade962c0291c7057bb1
- <chinhcomhut@gmail.com>
2019-10-03 16:20:49 +0700

    Class 2D and 3D

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(4.12f,  5.5f);
+        System.out.println(point2D.toString());
+        Point3D point3D = new Point3D(4.12f,5.3f,5.9f);
+        System.out.println(point3D.toString());
+    }
+}
